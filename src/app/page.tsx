69914536e17028d4079f2f44b33d9896a20b609e
Boris Stanar <bstanar96@gmail.com>
2025-12-03 14:55:40 +0100

    Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
+import { LogoutButton } from "./logout";
 
 const Page = async () => {
-  const users = await caller.getUsers();
+   await requireAuth();
 
-  return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(users)}
-    </div>
-  );
-}
+   const data = await caller.getUsers();
+
+   return (
+      <div className=" min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+         Protected server component
+         <div>
+            {JSON.stringify(data, null, 2)}
+         </div>
+        <LogoutButton/>
+      </div>
+   )
+};
 
 export default Page;
